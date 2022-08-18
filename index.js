c5df85ca2a42e7fa864980bb5802f18343565fc3
Davydov Konstantin <olec85@bk.ru>
2022-08-18 18:49:19 +0300

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const nameProduct = 'audi';//Наименование товара
+const amountProduct = 12;//Количество товара
+const categoryProduct = 'auto';//Категория товара
+const priceProduct = 1500;//Цена товара
+
+console.log(nameProduct);
+
+const sum = priceProduct * amountProduct;
+
+console.log(sum);
